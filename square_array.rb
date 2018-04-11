2cb74e820c8cd397bb4decdd45406bfb2ddf40ea
Eric Fiorani <efiorani305@gmail.com>
2018-04-11 15:07:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   return new_array
 end
 
-def square_array(array)
-  array.collect {|i| i ** 2}
-  return array
-end
+# def square_array(array)
+#   array.collect {|i| i ** 2}
+#   return array
+# end
